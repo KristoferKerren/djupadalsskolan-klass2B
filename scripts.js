d2d74ad56db175ea1af97a41e1d1df56731a3e8d
Kristofer Kerrén <kristofer.kerren@gmail.com>
2024-12-22 14:15:54 +0100

    Lite förfiningar

--- scripts.js
+++ scripts.js
@@ -6,4 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
       "HA! Du klickade fast du inte fick, nu får du ge mig extra många och fina julklappar! 🤣🤣🤣🤣"
     );
   });
+
+  const bytFärgKnapp = document.getElementById("byt-färg-knapp");
+  bytFärgKnapp.addEventListener("click", () => {
+    if (document.body.style.backgroundColor === "pink") {
+      document.body.style.backgroundColor = "rgb(27, 235, 27)";
+    } else {
+      document.body.style.backgroundColor = "pink";
+    }
+  });
+
+  const bytRubrikFärgKnapp = document.getElementById("byt-rubrikfärg-knapp");
+  bytRubrikFärgKnapp.addEventListener("click", () => {
+    if (document.body.classList.contains("rubrikfärg-ska-vara-lila")) {
+      document.body.classList.remove("rubrikfärg-ska-vara-lila");
+    } else {
+      document.body.classList.add("rubrikfärg-ska-vara-lila");
+    }G
+  });
 });
